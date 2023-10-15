838f071bff60a76c59eb207e977250c7bd27023b
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-10-15 08:22:39 -0400

    test

--- src/index.php
+++ src/index.php
@@ -20,14 +20,14 @@ return function ($context) {
         ]);
     }
 
-    /*$headers = $context->req->getallheaders();
-    $token = explode(' ', $headers["authorization"] ?? '')[1] ?? '';
+
+    $token = explode(" ", ($context->req->headers['Authorization'] ?? ''))[1];
 
     try {
-        $decoded = JWT::decode($token, $VONAGE_API_SIGNATURE_SECRET, array('HS256'));
+        $decoded = JWT::decode($token, $VONAGE_API_SIGNATURE_SECRET,'HS256');
     } catch (Exception $e) {
         $context->error('JWT validation error: ' . $e->getMessage());
-    }*/
+    }
 
     throw_if_missing($context->req->body, ['from','text']);
 
