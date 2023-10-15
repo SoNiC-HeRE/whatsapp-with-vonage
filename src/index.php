f109b87d18c2bc7be4a1a3e267272f9ff82a4cfc
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-10-15 06:54:02 -0400

    update

--- src/index.php
+++ src/index.php
@@ -4,9 +4,17 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/utils.php');
 
 return function ($context) {
+    throw_if_missing($_ENV, [
+        'VONAGE_API_KEY',
+        'VONAGE_API_SECRET',
+        'VONAGE_API_SIGNATURE_SECRET',
+        'VONAGE_WHATSAPP_NUMBER',
+    ]);
+
     if ($context->req->method === 'GET') {
-        // Send a response with the res object helpers
-        // `res.send()` dispatches a string back to the client
-        return $context->res->send('Checking');
+        return $context->res->send(get_static_file('index.html'), 200, [
+            'Content-Type' => 'text/html; charset=utf-8',
+        ]);
     }
+
 };
