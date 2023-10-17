676cb1f1e2549e536abd76b6286fcd16e57c4733
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-10-17 17:13:02 -0400

    Refine

--- src/index.php
+++ src/index.php
@@ -26,8 +26,7 @@ return function ($context) {
     $payload = base64_decode($jwtToken[1]);
     $decodedPayload = json_decode($payload, true);
 
-    $key = $_ENV['VONAGE_API_SIGNATURE_SECRET'];
-    $decoded = JWT::decode($jwtParts, new Key($key, 'HS256'));
+    $decoded = JWT::decode($jwtParts, new Key($_ENV['VONAGE_API_SIGNATURE_SECRET'], 'HS256'));
 
     if(hash("sha256",$context->req->bodyRaw) !== $decodedPayload["payload_hash"]){
         $context->res->json([
