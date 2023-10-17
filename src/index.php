3597fe5f889afaf76e6799fa5538e2b2c6d8cbec
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-10-17 17:46:12 -0400

    Fix

--- src/index.php
+++ src/index.php
@@ -23,8 +23,10 @@ return function ($context) {
     $authorizationHeader = isset($context->req->headers["authorization"]) ? $context->req->headers["authorization"] : "";
     $jwtParts = explode(" ", $authorizationHeader)[1] ?? "";
     $jwtToken = explode(".", $jwtParts);
+
     $decoded = JWT::decode($jwtToken, new Key($_ENV['VONAGE_API_SIGNATURE_SECRET'], 'HS256'));
-    if(hash("sha256",$context->req->bodyRaw) !== $decoded["payload_hash"]){
+
+    if(hash("sha256",$context->req->bodyRaw) !== $decoded[0]["payload_hash"]){
         $context->res->json([
             'ok' => false,
             'error' => "Payload Mismatch"
